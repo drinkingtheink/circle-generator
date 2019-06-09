b0dd7901d776c776f71b09a2bca81f7e49608d42
Jason M Harrison <jason.m.harrison@gmail.com>
2019-06-09 01:13:07 -0500

    Now using color palettes.

--- src/containers/IlloContainer.js
+++ src/containers/IlloContainer.js
@@ -1,34 +1,40 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import CircleSVG from '../components/CircleSVG';
+import palettes from '../palettes';
 
-const maxCircles = 35;
-const minCircles = 8;
+const maxCircles = 50;
+const minCircles = 15;
 
 class IlloContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      circlesModel: null
+      circlesModel: null,
+      currentPalette: null
     }
     this.generateCircleModel = this.generateCircleModel.bind(this);
   }
 
-  // generate number of circles between 10 - 25
-  // generate width
-  // generate top
-  // generate right
-  // generate opacity
-  // generate z-index
-  // generate fill
-
   generateCircleModel() {
+    this.getPalette();
     let circleCount = this.getRandomInt(minCircles, maxCircles);
     let circlesModel = [];
     let i;
     for (i = 0; i < circleCount; i++) { 
-      circlesModel.push({ circle: true })
+      circlesModel.push({ circle: true, key: `circle-${i++}` })
     }
     this.setState({ circlesModel: circlesModel })
+  }
+
+  getPalette() {
+    let randomPalette = palettes[Math.floor(Math.random()*palettes.length)];
+    this.setState({ currentPalette: randomPalette });
+  }
+
+  getFill() {
+    let currentPalette = this.state.currentPalette || [];
+    let randomHex = currentPalette ? currentPalette[Math.floor(Math.random()*currentPalette.length)] : [];
+    return randomHex;
   }
 
   getRandomInt(min, max) {
@@ -43,18 +49,20 @@ class IlloContainer extends Component {
 
   render() {
     return (
-      <main>
+      <main className="illustration-container">
         <h1>Let's Draw!</h1>
 
-        <button className="regenerate" onClick={this.generateCircleModel}>RE-GENERATE</button>
+        <button className="regenerate" onClick={this.generateCircleModel}>REGENERATE</button>
 
-        {this.state.circlesModel.map((item, index) => (
+        {this.state.circlesModel.map((circle, index) => (
           <CircleSVG 
             top={`${this.getRandomInt(-100, 100)}%`} 
             right={`${this.getRandomInt(-100, 100)}%`} 
             opacity={`.${this.getRandomInt(1, 9)}`} 
-            width={`${this.getRandomInt(50, 1000)}px`} 
+            width={`${this.getRandomInt(30, 800)}px`} 
             zIndex={`${this.getRandomInt(1, 10)}`} 
+            fill={this.getFill()}
+            key={circle.key}
           />
         ))}
         
---
+++ src/palettes/index.js
@@ -0,0 +1,28 @@
+const palettes = [
+  [
+    // https://coolors.co/f3a32f-1c3569-f8f9fa-25ced1-177e89
+    '#F3A32F',
+    '#1C3569',
+    '#F8F9FA',
+    '#25CED1',
+    '#177E89'
+  ],
+  [
+    // https://coolors.co/ceb43d-f2d59d-54998d-336c7f-193266
+    '#CEB43D',
+    '#F2D59D',
+    '#54998D',
+    '#336C7F',
+    '#193266'
+  ],
+  [
+    // https://coolors.co/app/f8ffe5-1b9aaa-ef476f-ffc43d-32e875
+    '#F8FFE5',
+    '#1B9AAA',
+    '#EF476F',
+    '#FFC43D',
+    '#32E875'
+  ]
+]
+
+export default palettes;
